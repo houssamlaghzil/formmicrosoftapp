970ca302e09130d0ae5bb5e8382ae40845b78279
houssam <houssam.laghzil@edu.itescia.fr>
2022-07-29 11:20:09 +0200

    firbase deploy and app android

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
-import Iframe from 'react-iframe'
+import React from 'react';
+import {WebView} from "react-native-webview";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Iframe width="100%" height="100%"
-    src="https://forms.office.com/Pages/ResponsePage.aspx?id=-dLyB8mx6EewDKWQPMYedMPoWy6UfSdFv7xPndjQqGpUM1NCUU0xTVRUM1lXSThSTEc5VFlSNzBGNC4u&embed=true"
-    frameBorder="0" marginWidth="0" marginHeight="0" style="border: none; max-width:100%; max-height:100vh"
-    allowFullScreen webkitallowfullscreen mozallowfullscreen msallowfullscreen/>
+        <WebView source ={{uri:'https://www.connect-street.fr/'}}
+                 style={{ marginTop: '10 %' }}
+                 sharedCookiesEnabled={true}
+              third PartyCookiesEnabled={true}
+        />
       </header>
     </div>
   );
